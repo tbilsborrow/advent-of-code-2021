6f8cc33cefaa0d349b24cd42390678c58b184d3f
Todd Bilsborrow <tbilsborrow@users.noreply.github.com>
2021-12-01 09:20:56 -0800

    use built in window function

--- src/main/kotlin/2021-1-1.kts
+++ src/main/kotlin/2021-1-1.kts
@@ -1,4 +1,4 @@
-val lines = java.io.File("../resources/input-1.txt").readLines()
+val lines = java.io.File("../resources/input-1.txt").readLines().asSequence()
 
 val r = lines
     .map { it.toInt() }
--- src/main/kotlin/2021-1-2.kts
+++ src/main/kotlin/2021-1-2.kts
@@ -1,26 +1,10 @@
-val lines = java.io.File("../resources/input-1.txt").readLines()
+val lines = java.io.File("../resources/input-1.txt").readLines().asSequence()
 
 val r = lines
     .map { it.toInt() }
-    .zipToTriples()
-    .map { it.first + it.second + it.third }
+    .windowed(3)
+    .map { it.sum() }
     .zipWithNext()
     .count { it.second > it.first }
 
 println(r)
-
-fun <T> Iterable<T>.zipToTriples(): Sequence<Triple<T, T, T>> {
-    return sequence result@ {
-        val iterator = iterator()
-        if (!iterator.hasNext()) return@result
-        var a = iterator.next()
-        if (!iterator.hasNext()) return@result
-        var b = iterator.next()
-        while (iterator.hasNext()) {
-            val c = iterator.next()
-            yield(Triple(a, b, c))
-            a = b
-            b = c
-        }
-    }
-}
